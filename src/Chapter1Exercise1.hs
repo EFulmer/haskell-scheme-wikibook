942154807cec2f411b8d4cbf7490199892b68d84
Eric Fulmer <EFulmer@gmail.com>
2016-04-23 16:35:31 -0400

    chapter 1 ex 1 solved

---
+++ src/Chapter1Exercise1.hs
@@ -0,0 +1,12 @@
+-- Chapter1Exercise1.hs
+module Chapter1Ex1 where
+import System.Environment
+
+-- Exercise 1:
+-- Change the program so it reads *two* arguments from the command line, and
+-- prints out a message using both of them
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn ("Hello, " ++ args !! 0 ++ " and hello, " ++ args !! 1)
