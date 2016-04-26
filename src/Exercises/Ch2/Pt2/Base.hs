6b7d6d7bd819465caa10ec2efdd807e6185a9188
Eric Fulmer <eric.r.fulmer@gmail.com>
2016-04-26 16:59:53 -0400

    source directly from ch2 pt2 (recursive parsers)

---
+++ src/Exercises/Ch2/Pt2/Base.hs
@@ -0,0 +1,96 @@
+-- Base.hs
+module Base where
+
+import Control.Monad
+import System.Environment
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal -- (a b c ... . z)
+             | Number Integer
+             | String String
+             | Bool Bool
+
+-- Begin helper functions (functions which don't return `LispVal`s):
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+-- End helper functions.
+
+-- Begin Lisp parsing functions:
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"") -- 0 or more non-doublequote characters.
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+-- End Lisp parsing functions.
+
+-- Start recursive parsers:
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  hd <- endBy parseExpr spaces
+  tl <- char '.' >> spaces >> parseExpr
+  return $ DottedList hd tl
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+-- End recursive parsers.
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+         <|> parseQuoted
+         -- TODO maybe refactor this into a general parseAnyList?
+         -- I just don't like the ro and char actions included amidst the
+         -- self-contained parse[Atom/String/Number/Quoted] functions.
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
+
+
+-- Begin "main"/"driver" functions:
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
+
+main :: IO ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn (readExpr expr)
+
+-- End "main"/"driver" functions.
+
